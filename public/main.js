71806286c94518bf5402dd8f9135294bad3fc80e
Whitley Beers <whitleybeers@gmail.com>
2022-11-15 17:34:20 -0600

    after several grueling hours, finally made it work!

--- public/main.js
+++ public/main.js
@@ -3,23 +3,84 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
+// render to dom
+const renderToDOM = (divId, content) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = content;
 };
 
-init();
+// clear dom
+const clearDom = () => {
+  document.querySelector('#joke-setup').innerHTML = '';
+  document.querySelector('#joke-delivery').innerHTML = '';
+  document.querySelector('#jokeBtnDiv').innerHTML = '';
+  document.querySelector('#punchlineBtnDiv').innerHTML = '';
+  document.querySelector('#resetBtnDiv').innerHTML = '';
+};
+
+// html structure
+const htmlStructure = () => {
+  document.querySelector('#app').innerHTML = '<div id="joke-setup"></div><div id="joke-delivery"></div><div id="jokeBtnDiv"></div><div id="punchlineBtnDiv"></div><div id="resetBtnDiv"></div>';
+};
+
+// buttons
+const jokeBtn = () => {
+  const domString = '<button class="btn btn-primary" id="jokeBtn">Get a joke</button>';
+  renderToDOM('#jokeBtnDiv', domString);
+};
+
+const punchlineBtn = () => {
+  const domString = '<button class="btn btn-primary" id="jokeBtn">Get the punchline</button>';
+  renderToDOM('#punchlineBtnDiv', domString);
+};
+
+const newJokeBtn = () => {
+  const domString = '<button class="btn btn-danger" id="jokeBtn">Get a new joke</button>';
+  renderToDOM('#resetBtnDiv', domString);
+};
+
+// api call
+const endPoint = 'https://v2.jokeapi.dev/joke/Spooky?safe-mode&type=twopart';
+
+const getJoke = () => new Promise((resolve, reject) => {
+  fetch(endPoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object(data)))
+    .catch(reject);
+});
+
+// events
+const makeJoke = () => {
+  getJoke().then((object) => {
+    document.querySelector('#jokeBtnDiv').addEventListener('click', () => {
+      renderToDOM('#joke-setup', `${object.setup}`);
+      document.querySelector('#jokeBtnDiv').innerHTML = '';
+      punchlineBtn();
+    });
+    document.querySelector('#punchlineBtnDiv').addEventListener('click', () => {
+      renderToDOM('#joke-delivery', `${object.delivery}`);
+      document.querySelector('#punchlineBtnDiv').innerHTML = '';
+      newJokeBtn();
+    });
+    document.querySelector('#resetBtnDiv').addEventListener('click', () => {
+      clearDom();
+      htmlStructure();
+      jokeBtn();
+      makeJoke();
+    });
+  });
+};
+
+// start App
+const startApp = () => {
+  htmlStructure();
+  jokeBtn();
+  makeJoke();
+};
+
+startApp();
